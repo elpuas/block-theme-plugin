1b8d636a74eea9a7660f6ee79c6d5eff3abbc83c
Alfredo Navas <elpuas@gmail.com>
2024-01-29 07:51:27 -0600

    Latest Updates
     Changes to be committed:
    	modified:   inc/register-blocks.php
    	modified:   inc/register-patterns.php

--- inc/register-blocks.php
+++ inc/register-blocks.php
@@ -1,13 +1,16 @@
 <?php
 /**
 * Registers the block using the metadata loaded from the `block.json` file.
-* Behind the scenes, it registers also all assets so they can be enqueued
-* through the block editor in the corresponding context.
 *
-* @see https://developer.wordpress.org/reference/functions/register_block_type/
+* @package theme-test
 */
+
+/**
+ * Initializes the theme test custom blocks.
+ */
 function theme_test_theme_test_block_init() {
 	register_block_type( plugin_dir_path( __DIR__ ) . '/build/block-example' );
 	register_block_type( plugin_dir_path( __DIR__ ) . '/build/block-example-copy' );
 }
+
 add_action( 'init', 'theme_test_theme_test_block_init' );
--- inc/register-patterns.php
+++ inc/register-patterns.php
@@ -1,26 +1,46 @@
 <?php
+/**
+ * Register Patterns and Pattern Category
+ *
+ * @package theme-test
+ */
+
+
+
+/**
+ * Registers a custom pattern category.
+ *
+ * This function is used to register a custom pattern category for the Theme Test plugin.
+ *
+ * @since 1.0.0
+ */
 function register_custom_pattern_category() {
-    register_block_pattern_category(
-        'theme-test-category',
-        array(
-            'label' => 'Theme Test Category',
-        )
-    );
+	register_block_pattern_category(
+		'theme-test-category',
+		array(
+			'label' => 'Theme Test Category',
+		)
+	);
 }
 
 add_action( 'init', 'register_custom_pattern_category' );
 
 
+/**
+ * Registers a custom block pattern.
+ *
+ * @return void
+ */
 function register_pattern_example() {
-    register_block_pattern(
-        'theme-test/pattern-example',
-        array(
-            'title'       => 'Pattern Example',
-            'description' => 'Description of your custom pattern',
-            'categories'    => [ 'theme-test-category' ],
-            'content'     => include plugin_dir_path(__DIR__) . 'patterns/pattern-example.php',
-        )
-    );
+	register_block_pattern(
+		'theme-test/pattern-example',
+		array(
+			'title'       => 'Pattern Example',
+			'description' => 'Description of your custom pattern',
+			'categories'  => [ 'theme-test-category' ],
+			'content'     => include plugin_dir_path( __DIR__ ) . 'patterns/pattern-example.php',
+		)
+	);
 }
 
 add_action( 'init', 'register_pattern_example' );
